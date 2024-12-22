aa0ded7e8833625685e43a83422e12c7f800e826
KilaniMaryem <mariem.kilani@insat.ucar.tn>
2024-12-22 13:34:05 +0100

    c

--- utils/custom.py
+++ utils/custom.py
@@ -1,11 +1,24 @@
 css_code: str = """
     <style>
-    section[data-testid="stSidebar"] > div > div:nth-child(2) {
-        padding-top: 0.75rem !important;
+    
+    .stAppHeader[data-testid="stHeader"] {
+    display: none !important;
+}
+
+    section[data-testid="stSidebar"] > div {
+        display: none;
+        padding: 1rem !important;
     }
     
+    
     section.main > div {
-        padding-top: 64px;
+        padding: 2rem 1.5rem;
+    }
+    
+
+    body {
+        font-family: 'Arial', sans-serif;
+        line-height: 1.6;
     }
     </style>
 """
